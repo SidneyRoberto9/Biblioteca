cd833c2fc92c29cb729a77ede8f0970478f53130
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-06-18 01:58:51 -0300

    ✨ feat: card basic styled

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,7 +1,29 @@
 import './card.scss';
 
 import React from 'react';
+import { AiFillHeart } from 'react-icons/ai';
 
 export const Card = () => {
-  return <div>C</div>;
+  return (
+    <div className="card">
+      <img
+        src="http://books.google.com/books/content?id=kYE8EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+        alt=""
+      />
+
+      <span className="title">
+        League of Legends: Reinos de RuneterraLeague of Legends: Reinos de
+        RuneterraLeague of Legends: Reinos de RuneterraLeague of Legends: Reinos
+        de RuneterraLeague of Legends: Reinos de RuneterraLeague of Legends:
+        Reinos de RuneterraLeague of Legends: Reinos de RuneterraLeague of
+        Legends: Reinos de RuneterraLeague of Legends: Reinos de RuneterraLeague
+        of Legends: Reinos de RuneterraLeague of Legends: Reinos de
+        RuneterraLeague of Legends: Reinos de Runeterra
+      </span>
+
+      <span className="favorite">
+        <AiFillHeart className="icon" />
+      </span>
+    </div>
+  );
 };
