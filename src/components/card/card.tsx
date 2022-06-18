873dcd27b64aa1fe51389f91a43f6219de2377ca
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-06-18 17:28:56 -0300

    📦 refactor: change img card size and nullisher img variable

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -3,20 +3,35 @@ import './card.scss';
 import React from 'react';
 import { AiFillHeart } from 'react-icons/ai';
 
+import { Book, favorite } from '../../models/book.model';
+
 interface CardProps {
-  title: string;
-  img: string;
-  save: (title: string) => void;
+  book: Book;
+  save: (book: favorite) => void;
 }
 
 export const Card = (props: CardProps) => {
+  const { book, save } = props;
+
   return (
     <div className="card">
-      <img src={props.img} alt={props.title} />
+      <img
+        src={book.volumeInfo.imageLinks?.thumbnail || ''}
+        alt={book.volumeInfo.title}
+      />
 
-      <span className="title">{props.title}</span>
+      <span className="title">{book.volumeInfo.title}</span>
 
-      <span className="favorite" onClick={() => props.save(props.title)}>
+      <span
+        className="favorite"
+        onClick={() =>
+          save({
+            id: book.id,
+            title: book.volumeInfo.title,
+            selfLink: book.selfLink,
+          })
+        }
+      >
         <AiFillHeart className="icon" />
       </span>
     </div>
